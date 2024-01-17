16d2def8cf257de0af44c54b1dc65e111f3660d8
avidcreator <1@fernando.app>
2024-01-17 12:54:17 -0600

    Rename ContentView to NowView

---
+++ Tasky/Tasky/NowView.swift
@@ -0,0 +1,21 @@
+//
+//  NowView.swift
+//  Tasky
+//
+//  Created by Fernando Cervantes on 1/17/24.
+//
+
+import SwiftUI
+
+struct NowView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NowView()
+}
