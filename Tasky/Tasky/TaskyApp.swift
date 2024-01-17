aae5b7a3091236db72efca320613058e46e3b4e1
avidcreator <1@fernando.app>
2024-01-17 12:51:15 -0600

    Create project

---
+++ Tasky/Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Fernando Cervantes on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
