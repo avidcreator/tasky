eb5409d9cbd9a5f1bf57eef31243d2b11ec1f438
avidcreator <1@fernando.app>
2024-01-17 13:03:23 -0600

    Create TaskView

---
+++ Tasky/Tasky/Task View/TaskView.swift
@@ -0,0 +1,86 @@
+//
+//  TaskView.swift
+//  Tasky
+//
+//  Created by Fernando Cervantes on 1/17/24.
+//
+
+import SwiftUI
+import SymbolPicker
+
+struct TaskView: View {
+    @State var task: Task
+    @State var showSymbolPicker: Bool
+    @Binding var isShowingTaskView: Bool
+    @Binding var minuteGroupSelected: String?
+    @State var isEditing: Bool
+    
+    private var cancelButton: some View {
+        Button("Cancel") {
+            isShowingTaskView = false
+        }
+    }
+    
+    private var addButton: some View {
+        Button {
+            isShowingTaskView = false
+            minuteGroupSelected = nil
+        } label: {
+            Text("Add")
+        }
+    }
+    
+    private var form: some View {
+        Form {
+            Section {
+                HStack {
+                    Spacer()
+                    Button {
+                        showSymbolPicker = true
+                    } label: {
+                        SymbolView(systemName: task.symbol ?? "", size: .medium, style: .highlighted)
+                    }
+                    .sheet(isPresented: $showSymbolPicker) {
+                        SymbolPicker(symbol: $task.symbol)
+                    }
+                    Spacer()
+                }
+                NameView(placeholder: "name your task", name: $task.name)
+            }
+            .listRowBackground(Color.clear)
+            .listRowSeparator(.hidden)
+        }
+        .navigationTitle(isEditing ? "Edit task" : "New task")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            if !isEditing {
+                ToolbarItem(placement: .topBarLeading) {
+                    cancelButton
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    addButton
+                }
+            }
+        }
+        .onDisappear {
+            minuteGroupSelected = nil
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            if isEditing {
+                form
+            } else {
+                NavigationView {
+                    form
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    let sampleTask = Task(name: "Go for a swim", symbol: "figure.pool.swim", date: Date(), minuteGroup: .zeroToTen)
+    return TaskView(task: sampleTask, showSymbolPicker: false, isShowingTaskView: .constant(false), minuteGroupSelected: .constant(nil), isEditing: false)
+}
