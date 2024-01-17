7c1e130ea42ae332c5536dd3fabbfa452994b8ab
avidcreator <1@fernando.app>
2024-01-17 12:55:45 -0600

    Create Task

---
+++ Tasky/Tasky/Task.swift
@@ -0,0 +1,24 @@
+//
+//  Task.swift
+//  Tasky
+//
+//  Created by Fernando Cervantes on 1/17/24.
+//
+
+import Foundation
+
+final class Task {
+    var id = UUID().uuidString
+    var name: String
+    var symbol: String?
+    var date: Date
+    var minuteGroup: MinuteGroup
+    
+    init(id: String = UUID().uuidString, name: String, symbol: String?, date: Date, minuteGroup: MinuteGroup) {
+        self.id = id
+        self.name = name
+        self.symbol = symbol
+        self.date = date
+        self.minuteGroup = minuteGroup
+    }
+}
